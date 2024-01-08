ece17d0906a713f78a0a8557b37e51411f21a770
djp3k05 <79905373+djp3k05@users.noreply.github.com>
2024-01-08 18:33:50 +0200

    Update const.py

--- custom_components/sungrow/const.py
+++ custom_components/sungrow/const.py
@@ -14,12 +14,32 @@ MIN_TIME_BETWEEN_UPDATES = timedelta(seconds=5)
 
 SUNGROW_MPPT1_VOLTAGE = "mppt_1_voltage"
 SUNGROW_MPPT1_CURRENT = "mppt_1_current"
+SUNGROW_MPPT2_VOLTAGE = "mppt_2_voltage"
+SUNGROW_MPPT2_CURRENT = "mppt_2_current"
+SUNGROW_phase_a_voltage = "phase_a_voltage"
+SUNGROW_work_state_1 = "work_state_1"
+SUNGROW_internal_temperature = "internal_temperature"
+SUNGROW_total_reactive_power = "total_reactive_power"
+SUNGROW_array_insulation_resistance = "array_insulation_resistance"
+SUNGROW_work_state_2 = "work_state_2"
+SUNGROW_total_power_yields = "total_power_yields"
+SUNGROW_negative_voltage_to_the_ground = "negative_voltage_to_the_ground"
+SUNGROW_bus_voltage = "bus_voltage"
+SUNGROW_grid_frequency = "grid_frequency"
+SUNGROW_pid_work_state = "pid_work_state"
+SUNGROW_pid_alarm_code = "pid_alarm_code"
+SUNGROW_power_meter = "power_meter"
+SUNGROW_string_1_current = "string_1_current"
+SUNGROW_string_2_current = "string_2_current"
+SUNGROW_pid_recovery = "pid_recovery"
+SUNGROW_total_running_time = "total_running_time"
+SUNGROW_total_apparent_power = "total_apparent_power"
+
 
 # Unit is W
 SUNGROW_ENERGY_GENERATION = "energy_generation"
-# Unit is kWh
-SUNGROW_ENERGY_CONSUMPTION = "energy_consumption"
 
+SUNGROW_METER_POWER = "meter_power"
 SUNGROW_ARRAY1_ENERGY_GENERATION = "array1_energy_generation"
 SUNGROW_ARRAY1_VOLTAGE = "array1_voltage"
 SUNGROW_ARRAY1_CURRENT = "array1_current"
@@ -34,28 +54,6 @@ SUNGROW_LOAD_POWER_HYBRID = "load_power_hybrid"
 SUNGROW_EXPORT_POWER = "export_power"
 SUNGROW_EXPORT_POWER_HYBRID = "export_power_hybrid"
 
-SUNGROW_DAILY_IMPORT_ENERGY = "daily_import_energy"
-SUNGROW_TOTAL_IMPORT_ENERGY = "total_import_energy"
-
-# Battery charged from PV
-# TODO these arent real registers.
-SUNGROW_DAILY_BATTERY_CHARGE_PV_ENERGY = "daily_battery_charge_energy_from_pv"
-SUNGROW_TOTAL_BATTERY_CHARGE_PV_ENERGY = "total_battery_charge_energy_from_pv"
-
-# Battery charged from GRID
-SUNGROW_DAILY_BATTERY_CHARGE_GRID_ENERGY = "daily_charge_energy"
-SUNGROW_TOTAL_BATTERY_CHARGE_GRID_ENERGY = "total_charge_energy"
-
-SUNGROW_DAILY_BATTERY_DISCHARGE_ENERGY = "daily_battery_discharge_energy"
-SUNGROW_TOTAL_BATTERY_DISCHARGE_ENERGY = "total_battery_discharge_energy"
-
-SUNGROW_INVERTER_EFFICENCY = "inverter_efficency"
-
-# TODO these arent real registers.
-SUNGROW_DAILY_OUTPUT_ENERGY = "daily_output_energy"
-SUNGROW_TOTAL_OUTPUT_ENERGY = "total_output_energy"
-
-SUNGROW_GRID_FREQUENCY = "grid_frequency"
 
 SUNGROW_DAILY_PV_ENERGY = "daily_pv_generation"
 SUNGROW_TOTAL_PV_ENERGY = "total_pv_generation"
@@ -63,28 +61,19 @@ SUNGROW_TOTAL_PV_ENERGY = "total_pv_generation"
 SUNGROW_DAILY_DIRECT_ENERGY_CONSUMPTION = "daily_direct_energy_consumption"
 SUNGROW_TOTAL_DIRECT_ENERGY_CONSUMPTION = "total_direct_energy_consumption"
 
-SUNGROW_DAILY_EXPORT_ENERGY = "daily_export_energy"
 SUNGROW_TOTAL_EXPORT_ENERGY = "total_export_energy"
 
 # TODO these arent real registers. Use daily_pv_export, total_pv_export instead?
 SUNGROW_DAILY_EXPORT_ENERGY_FROM_PV = "daily_export_power_from_pv"
 SUNGROW_TOTAL_EXPORT_ENERGY_FROM_PV = "total_export_power_from_pv"
 
-SUNGROW_DAILY_EXPORT_ENERGY_FROM_BATTERY = "daily_export_energy"
-SUNGROW_TOTAL_EXPORT_ENERGY_FROM_BATTERY = "total_export_energy"
-
-SUNGROW_SELF_CONSUMPTION_OF_DAY = "self_consumption_of_day"
 
 SUNGROW_DAILY_POWER_YIELDS =  "daily_power_yields" # kWh
 
 SUNGROW_DAILY_PV_EXPORT = "daily_pv_export"
-SUNGROW_TOTAL_PV_EXPORT = "total_pv_export"
 
-SUNGROW_METER_POWER = "meter_power"
 SUNGROW_TOTAL_ACTIVE_POWER = "total_active_power"
 
-SUNGROW_STATE_BATTERY_CHARGING = "state_battery_charging"
-SUNGROW_STATE_BATTERY_DISCHARGING = "state_battery_discharging"
 SUNGROW_STATE_FEED_INTO_GRID = "state_feed_into_grid"
 SUNGROW_STATE_IMPORT_FROM_GRID = "state_import_from_grid"
 SUNGROW_STATE_LOAD_ACTIVE = "state_load_active"
